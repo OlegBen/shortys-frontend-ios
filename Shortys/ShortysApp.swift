809b0bcb58151d481ff4aa32db1b3712425184d7
Oleg Ben <o.ben@ideas-world.com>
2021-12-22 16:48:11 +0300

    Start project

---
+++ Shortys/ShortysApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortysApp.swift
+//  Shortys
+//
+//  Created by user on 22.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ShortysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
